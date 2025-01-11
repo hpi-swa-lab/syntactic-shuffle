a719d7f6eacb18c2e1d8492ccc3d0ea2bad00be9
Marius Dörbandt <github.marius@doerbandt.de>
2025-01-11 16:22:43 +0100

    implement player shoot logic with cards

--- player.gd
+++ player.gd
@@ -1,8 +1,4 @@
 extends CharacterBody2D
 
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_left_click"):
-		$Pistol.shoot()
-
 func take_damage(_damage: float):
 	queue_free()
