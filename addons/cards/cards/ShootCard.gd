7530b060b1a4de9f16c32508500ce791aa95ea94
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2025-01-11 15:17:35 +0100

    shoot card wip

---
+++ addons/cards/cards/ShootCard.gd
@@ -0,0 +1,16 @@
+@tool
+#thumb("Remove")
+extends Card
+
+func _ready() -> void:
+	super._ready()
+	setup("Shoot",
+		"Shoot a gun!",
+		Card.Type.Effect,
+		[ObjectInputSlot.create("gun"), InputSlot.create(1)])
+	on_invoke_input(invoke)
+
+func invoke(obj):
+	var input_gun = get_object_input_slot().get_object(self)
+	print(input_gun)
+	#if input_gun: input_gun.shoot()
