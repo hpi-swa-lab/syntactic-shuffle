68249dcdd26bbe0e2b51f0025f2b7a21bc8fa425
Marius Dörbandt <github.marius@doerbandt.de>
2025-01-11 11:36:21 +0100

    add pistol, health and rotation controls to enemy

--- enemy.gd
+++ enemy.gd
@@ -1,4 +1,19 @@
 extends Node2D
 
+@export var health = 20
+
+var noise = FastNoiseLite.new()
+var time_alive = 0
+
+func take_damage(damage: float):
+	health -= damage
+	if health <= 0:
+		queue_free()
+
 func _on_fov_spots(object: Object) -> void:
-	print(object)
+	look_at(object.global_position)
+	$Pistol.shoot()
+
+func _process(delta: float) -> void:
+	rotation += noise.get_noise_1d(time_alive) * 0.5
+	time_alive += delta
