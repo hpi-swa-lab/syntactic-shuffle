05d5b87c8ac7f15c087acff37fad36c60a264c76
Marius Dörbandt <github.marius@doerbandt.de>
2025-01-11 16:20:35 +0100

    fix ShootCard to take zero arguments

--- addons/cards/cards/ShootCard.gd
+++ addons/cards/cards/ShootCard.gd
@@ -7,9 +7,9 @@ func _ready() -> void:
 	setup("Shoot",
 		"Shoot a gun!",
 		Card.Type.Effect,
-		[ObjectInputSlot.create("gun"), InputSlot.create(1)])
+		[ObjectInputSlot.create("gun"), InputSlot.create(0)])
 	on_invoke_input(invoke)
 
-func invoke(obj):
+func invoke():
 	var input_gun = get_object_input_slot().get_object(self)
 	if input_gun: input_gun.shoot()
