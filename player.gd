84b4f8263a9e2e06dce0decce0c9dc0cd74fb6ec
Marius Dörbandt <github.marius@doerbandt.de>
2025-01-11 11:35:47 +0100

    add player

---
+++ player.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("ui_left_click"):
+		$Pistol.shoot()
+
+func take_damage(_damage: float):
+	queue_free()
