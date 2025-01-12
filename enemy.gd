287e9da18aca3274572c23ea7df95ff41f45f37e
Marius Dörbandt <github.marius@doerbandt.de>
2025-01-12 16:04:08 +0100

    drop cards on enemy death

--- enemy.gd
+++ enemy.gd
@@ -5,7 +5,16 @@ extends Node2D
 func take_damage(damage: float):
 	health -= damage
 	if health <= 0:
-		queue_free()
+		die()
 
 func show_brain(_show: bool):
 	$brain.visible = _show
+
+func die():
+	queue_free()
+	for child in $brain/CardBoundary.get_children():
+		if child is Card:
+			child.get_parent().remove_child(child)
+			Spawn.spawn(child)
+			child.global_position = global_position + Vector2(randf() - 0.5, randf() - 0.5) * 200
+			child.locked = false
