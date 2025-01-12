9de590c6863007d50c8c06511781fba678a4b6fe
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2025-01-12 17:03:38 +0100

    fix death screen

--- death_screen.gd
+++ death_screen.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 func _ready() -> void:
+	await get_tree().create_timer(1).timeout
 	Globals.play_sound(preload("res://resources/sounds/Failure.wav"))
 	
 	await get_tree().create_timer(2.5).timeout
