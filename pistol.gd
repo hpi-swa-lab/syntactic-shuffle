29de4c2cc09bdedf22025ff90ca99c2d97099783
Marius Dörbandt <github.marius@doerbandt.de>
2025-01-12 16:58:55 +0100

    add pistol cooldown

--- pistol.gd
+++ pistol.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 @export var owner_group: String
 
+var last_shot = 0
+@export var cooldown_msec = 1000
+
 func shoot():
+	if Time.get_ticks_msec() - last_shot < cooldown_msec:
+		return
+	last_shot = Time.get_ticks_msec()
 	var bullet = preload("res://bullet.tscn").instantiate()
 	Spawn.spawn(bullet)
 	bullet.global_position = global_position
