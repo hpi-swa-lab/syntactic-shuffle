8132d71d561e5df7290ca68708583ac285f5d43a
Tom Beckmann <tomjonabc@gmail.com>
2025-01-12 12:02:14 +0100

    prepare brains

---
+++ addons/cards/CardBoundary.gd
@@ -0,0 +1,9 @@
+@tool
+extends Node
+class_name CardBoundary
+
+static func get_card_boundary(card: Card):
+	var b = G.closest_parent_that(card, func (n): return n is CardBoundary)
+	if not b:
+		return card.get_tree().root
+	return b
---
+++ addons/cards/CardProxy.gd
@@ -0,0 +1,16 @@
+@tool
+extends Sprite2D
+class_name CardProxy
+
+@export var proxy_target: Node:
+	get:
+		return proxy_target
+	set(v):
+		proxy_target = v
+		for group in get_groups():
+			remove_from_group(group)
+		for group in v.get_groups():
+			add_to_group(group)
+
+func _ready() -> void:
+	texture = preload("res://resources/effects/shieldSciFi.png")
