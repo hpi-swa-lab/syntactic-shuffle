a477297e6b1c5acedb1080a7fcfeb97c6881acd1
Marius Dörbandt <github.marius@doerbandt.de>
2025-01-12 12:07:10 +0100

    add turret

--- auto_shooter.gd
+++ auto_shooter.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var health = 20
+@export var gun: Node
 
 var noise = FastNoiseLite.new()
 var time_alive = 0
@@ -14,7 +15,7 @@ func _on_fov_spots(object: Node2D) -> void:
 	if not object.is_in_group("player"):
 		return
 	look_at(object.global_position)
-	$Pistol.shoot()
+	gun.shoot()
 
 func _process(delta: float) -> void:
 	rotation += noise.get_noise_1d(time_alive) * 0.5
