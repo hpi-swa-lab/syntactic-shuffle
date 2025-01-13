e7e265f67e6088b15959d346a8039ea11458eb6e
Tom Beckmann <tomjonabc@gmail.com>
2025-01-13 09:46:23 +0100

    begin cleanup

--- addons/cards/cards/PressF.gd
+++
@@ -1,7 +0,0 @@
-@tool
-#thumb("press_F.png")
-extends Card
-
-func _ready() -> void:
-	super._ready()
-	setup("Respect", "", Card.Type.Trigger, [])
--- death_screen.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	await get_tree().create_timer(1).timeout
-	Globals.play_sound(preload("res://resources/sounds/Failure.wav"))
-	
-	await get_tree().create_timer(2.5).timeout
-	get_tree().change_scene_to_file("res://game.tscn")
