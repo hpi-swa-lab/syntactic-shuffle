9ef5f5bd98ae0c4d2ce926c4cecebaefeb50f885
Tom Beckmann <tomjonabc@gmail.com>
2025-01-21 22:18:27 +0100

    add a forward-trigger card

---
+++ addons/cards/cards/ForwardTriggerCard.gd
@@ -0,0 +1,16 @@
+@tool
+extends Card
+class_name ForwardTriggerCard
+
+func s():
+	title("Forward Trigger")
+	description("Receives any data input, discards the data, and emits a trigger.")
+	icon("forward.png")
+	
+	var out_card = OutCard.data()
+	
+	var code_card = CodeCard.create([["in", any()]], {"out": trg()}, func (card): card.output("out", []))
+	code_card.c(out_card)
+	
+	var in_card = InCard.data(any())
+	in_card.c_named("in", code_card)
