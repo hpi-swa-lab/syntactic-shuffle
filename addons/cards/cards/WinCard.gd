b18fc4199b8e3dec6154d0fb83510b9051002fcd
Marius Dörbandt <github.marius@doerbandt.de>
2025-01-23 16:18:17 +0100

    add confetti

--- addons/cards/cards/WinCard.gd
+++ addons/cards/cards/WinCard.gd
@@ -8,7 +8,14 @@ func s():
 	icon(preload("res://addons/cards/icons/bool.png"))
 	
 	var code_card = CodeCard.create([["in", trg()]], {}, func(card):
-		print("you win!"))
+		confetti.emitting = true)
 	
 	var in_card = InCard.trigger()
 	in_card.c_named("in", code_card)
+
+var confetti: GPUParticles2D
+
+func _ready() -> void:
+	super._ready()
+	confetti = preload("res://addons/cards/confetti.tscn").instantiate()
+	add_child(confetti)
