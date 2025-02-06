a3e857426919b4e81835321395f2dea15cdc3b5c
Tom Beckmann <tomjonabc@gmail.com>
2025-02-06 06:07:10 +0100

    fix tests, pull-only loop

--- addons/cards/cards/PullOnlyCard.gd
+++ addons/cards/cards/PullOnlyCard.gd
@@ -7,9 +7,9 @@ func v():
 	description("")
 	icon_data("iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAGFJREFUOI3dkMEOwCAMQsv+/5/fTibMudrG27g1AgIC4gRXlSiJ1V02ADREkgAUEaFuBRe3ErjY66QGTvSfH3WyCoPkkV+cYTCvPOPLpLVBmmD5WKiwTZCJtwkqON7gBwY3v749D22wXEcAAAAASUVORK5CYII=")
 	container_size(Vector2(2000.0, 1600.0))
-	allow_cycles()
 
 func s():
+	allow_cycles()
 	var in_card = InCard.data(any())
 	in_card.position = Vector2(263.7976, 633.5124)
 	var out_card = OutCard.remember()
